b17c249cf187791942bcfd50c94a2d598b701839
adason <tdadason@gmail.com>
2017-09-15 17:47:38 -0700

    Add script to download and prepare data

---
+++ download_data.sh
@@ -0,0 +1,8 @@
+mkdir data
+cd data
+
+# The German-English translation task of the IWSLT evaluation campaign
+mkdir iwslt
+cd iwslt
+wget http://phontron.com/data/iwslt-en-de-preprocessed.tar.gz
+tar -xf iwslt-en-de-preprocessed.tar.gz
