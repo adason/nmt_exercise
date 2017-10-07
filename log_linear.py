27935c647e4fc9a3bf1d55702a3f59533040b313
You-Cyuan Jhang <yjhang@castlighthealth.com>
2017-10-07 00:11:11 -0700

    Reorganize methods

--- log_linear.py
+++ log_linear.py
@@ -1,6 +1,7 @@
 """ Implement Log-linear Model in Chapter 4.
 """
 import collections
+import time
 
 import numpy as np
 
@@ -47,39 +48,6 @@ def slide_window(arr, n):
         yield arr[i:i+n]
 
 
-# def binarize_toksents(toksents, n_vocab):
-#     """ Split the tokens within a widow into (x, y) pairs, encoded in sparse matrix.
-#         e_t => y
-#         e^{t-1}_{t-n+1} => x
-#         x = [\phi(e_{t-n+1});\phi(e_{t-n+2});...;\phi(e_{t-2});\phi(e_{t-1})]
-#     """
-#     x_indptr, y_indptr = [0], [0]
-#     x_indices, y_indices = [], []
-#     x_data, y_data = [], []
-#     for sent in toksents:
-#         # x
-#         x_offset = 0
-#         for tok in sent[:-1]:
-#             x_indices.append(x_offset + tok)
-#             x_data.append(1)
-#             x_offset += n_vocab
-#         x_indptr.append(len(x_indices))
-#         # y
-#         tok = sent[-1]
-#         y_indices.append(tok)
-#         y_data.append(1)
-#         y_indptr.append(len(y_indices))
-#
-#     n_rows = len(toksents)
-#     n_x_cols = (len(toksents[0]) - 1) * n_vocab
-#     x = csr_matrix((x_data, x_indices, x_indptr), shape=(n_rows, n_x_cols), dtype=int)
-#
-#     n_y_cols = n_vocab
-#     y = csr_matrix((y_data, y_indices, y_indptr), shape=(n_rows, n_y_cols), dtype=int)
-#
-#     return x, y
-
-
 def split_toksents(toksents):
     """ Split the tokens within a widow into (x, y) pairs.
         e_t => y
@@ -96,26 +64,6 @@ def split_toksents(toksents):
     return x, y
 
 
-def linear_trans(x, w, b):
-    """ Compute the linear transformation.
-        x: token indicies; numpy array of shape (n_samples, n_grams)
-        w: weight matrix; numpy array of shape (n_grams, n_vocab, n_vocab)
-        b: bias; numpy array of shape (n_vocab,)
-        Formula: s_j = \sum_{j; x_j != 0} w_{.,j} * x_j + b_j
-    """
-    b = np.expand_dims(b, axis=0)
-    wx = []
-    for i in range(x.shape[0]):
-        x_ = x[i, :]  # (n_grams,)
-        w_ = [w[idx, :, j] for idx, j in enumerate(x_)]  # [(n_vocab,), (n_vocab,), ..]
-        wx_ = np.vstack(w_).sum(axis=0) # (n_vocab, )
-        wx.append(wx_)
-
-    wx = np.vstack(wx)
-
-    return wx + b
-
-
 def binarize(y):
     """ Convert multi-class y labels into one-hot encoded vectors.
     """
@@ -127,23 +75,6 @@ def binarize(y):
         bin_matrix[i, idx] = 1
 
     return bin_matrix
-
-
-def log_loss(y, p, eps=1e-15):
-    """ Compute the log loss (cross entropy loss).
-
-        y: multi-class labels (not one-hot encodings)
-        p: softmax transformed predictions.
-        N: number of samples
-        K: number of labels
-        loss = \sum_{i=0}^{N-1}\sum_{k=0}^{K-1} y_ik * log (p_ik)
-    """
-    y = binarize(y)
-    # Log loss is undefined for p=0 or p=1,
-    # so probabilities are clipped to (eps, 1-eps).
-    p = np.clip(p, eps, 1 - eps)
-
-    return -(y * np.log(p)).sum()
 
 
 def softmax(s, axis=None):
@@ -163,11 +94,104 @@ def softmax(s, axis=None):
     return p.squeeze()
 
 
-def total_loss(x, y, w, b):
-    s = linear_trans(x, w, b)
-    p = softmax(s, axis=1)
+def log_loss(y, p, eps=1e-15):
+    """ Compute the log loss (cross entropy loss).
 
-    return log_loss(y, p)
+        y: multi-class labels (not one-hot encodings)
+        p: softmax transformed predictions.
+        N: number of samples
+        K: number of labels
+        loss = \sum_{i=0}^{N-1}\sum_{k=0}^{K-1} y_ik * log (p_ik)
+    """
+    y = binarize(y)
+    # Log loss is undefined for p=0 or p=1,
+    # so probabilities are clipped to (eps, 1-eps).
+    p = np.clip(p, eps, 1 - eps)
+
+    return -(y * np.log(p)).sum()
+
+
+class LogLinearModel:
+    """ Log Linear Model.
+    """
+    def __init__(self, n_grams, n_vocab, random_state=None):
+        """ Initialize parameters.
+
+            :n_grams:
+            :n_vocab:
+            :random_state:
+        """
+        if random_state:
+            np.random.seed(random_state)
+
+        self.w = np.random.random_sample((n_grams), n_vocab, n_vocab)
+        self.b = np.random.random_sample((n_vocab,))
+
+    def fit(x, y, epochs=10, batch_size=32):
+        """ Fit the parameters.
+        """
+        loss_history = [np.Inf]
+        for e in epochs:
+            print("Training epoch {}".format(e))
+            loss = self._fit_one_epoch(x, y, batch_size)
+            loss_history.append(loss)
+            print("Done training epoch: {}. Loss: {}".format(e, loss))
+
+            if abs(loss_history[-2] - loss_history[-1]) < 1e-6:
+                break
+
+        return loss_history
+
+    def _fit_one_epoch(x, y, batch_size, eta=0.1):
+        """ Fit one epoch.
+        """
+        y_onehot = binarize(y)
+
+        losses = []
+        shuffled_idx = np.random.permutation(x.shape[0])
+        split_idx = [batch_size*(i+1) for i in range(int(x.shape[0] / batch_size))]
+        for batch_idx in np.split(shuffled_idx, split_idx):
+            x_bat = np.take(x, batch_idx, axis=0)
+            y_bat = np.take(y, batch_idx, axis=0)
+            y_onehot_bat = np.take(y_onehot, batch_idx, axis=0)
+            p = softmax(linear_trans(x_bat, self.w, self.b), axis=1)
+            self.b -= eta * np.mean(p - y_onehot_bat, axis=0)
+            # self.w -= eta * x_bat * (p - y_onehot_bat) 
+
+            loss_bat = total_loss(x_bat, y_bat, self.w, self.b)
+            loss.append(loss_bat)
+
+        return np.sum(losses)
+
+    @staticmethod
+    def linear_trans(x, w, b):
+        """ Compute the linear transformation.
+
+            x: token indicies; numpy array of shape (n_samples, n_grams)
+            w: weight matrix; numpy array of shape (n_grams, n_vocab, n_vocab)
+            b: bias; numpy array of shape (n_vocab,)
+            Formula: s_j = \sum_{j; x_j != 0} w_{.,j} * x_j + b_j
+        """
+        b = np.expand_dims(b, axis=0)
+        wx = []
+        for i in range(x.shape[0]):
+            x_ = x[i, :]  # (n_grams,)
+            w_ = [w[idx, :, j] for idx, j in enumerate(x_)]  # [(n_vocab,), (n_vocab,), ..]
+            wx_ = np.vstack(w_).sum(axis=0) # (n_vocab, )
+            wx.append(wx_)
+
+        wx = np.vstack(wx)
+
+        return wx + b
+
+    @staticmethod
+    def total_loss(x, y, w, b):
+        """ Compute the total loss.
+        """
+        s = LogLinearModel.linear_trans(x, w, b)
+        p = softmax(s, axis=1)
+
+        return log_loss(y, p)
 
 
 def main():
@@ -186,24 +210,25 @@ def main():
     # print(train_toksents[0])
     # print(list(slide_window(train_toksents[0], 3)))
 
-    # x, y = binarize_toksents([[0, 1, 2],[2, 1, 0]], 3)
+    # x, y = binarize_toksents([[0, 1, 2],[2, 1, 0],[0, 2, 1]], 3)
     # print(x.toarray())
     # print(y.toarray())
 
-    x, y = split_toksents([[0, 1, 2],[2, 1, 0]])
-    print(x, y)
-    print(softmax([1,2,3]))
-
-    w = np.array([
-        [[0.2, 0.6, 0.3],
-         [0.5, 0.1, 0.4],
-         [0.3, 0.3, 0.3]],
-        [[0.1, 0.6, 0.4],
-         [0.6, 0.1, 0.4],
-         [0.3, 0.3, 0.2]]
-    ])
-    b = np.array([0.1, 0.1, 0.1])
-    print(linear_trans(x, w, b))
+    # x, y = split_toksents([[0, 1, 2],[2, 1, 0],[0, 2, 1]])
+    # print(x, y)
+    # print(softmax([1,2,3]))
+    #
+    # w = np.array([
+    #     [[0.2, 0.6, 0.3],
+    #      [0.5, 0.1, 0.4],
+    #      [0.3, 0.3, 0.3]],
+    #     [[0.1, 0.6, 0.4],
+    #      [0.6, 0.1, 0.4],
+    #      [0.3, 0.3, 0.2]]
+    # ])
+    # b = np.array([0.1, 0.1, 0.1])
+    # print(LogLinearModel.linear_trans(x, w, b))
+    # print(LogLinearModel.total_loss(x, y, w, b))
 
 
     # y = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
