a94a651b2b4ade03a8fd039364f64d915bc804bc
adason <tdadason@gmail.com>
2017-10-26 00:05:28 -0700

    Add script to download fastest vector embedding

--- download_data.sh
+++ download_data.sh
@@ -6,3 +6,11 @@ mkdir iwslt
 cd iwslt
 wget http://phontron.com/data/iwslt-en-de-preprocessed.tar.gz
 tar -xf iwslt-en-de-preprocessed.tar.gz
+cd ..
+
+# fasttext https://fasttext.cc pre-trained vector embedding
+mkdir fasttext
+cd fasttext
+wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki-news-300d-1M.vec.zip
+unzip wiki-news-300d-1M.vec.zip
+cd ..
